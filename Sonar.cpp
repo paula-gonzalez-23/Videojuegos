a6dbd0cd11667989d66aef80a3c5e5b516010e7d
Jesus1102 <jesus60302@gmail.com>
2024-03-27 11:47:15 -0500

    Termine hacerMovimientos

--- Sonar.cpp
+++ Sonar.cpp
@@ -2,12 +2,13 @@
 #include <time.h>
 #include <stdlib.h>
 #include <vector>
+#include <math.h>
 using namespace std;
 
 class Sonar{
 
     public:
-    int ANCHO = 60, ALTO = 15, random;
+    int ANCHO = 60, ALTO = 15, random, numCofres = 3, coorX,coorY;
     string tab1 [60][15] = {};
     
 
@@ -88,7 +89,80 @@ class Sonar{
         return coordCofres;
     }
 
-    string movimiento(){
+    string hacerMovimientos(vector <int> coordCofres,int x ,int y){
 
+        int limite = 100, pos = 0;
+        float distancia = 0;
+
+        for(int i = 1; i < 6; i +=2 ){
+            distancia = sqrt((coordCofres[i]-x) * (coordCofres[i]-x) + (coordCofres[i+1]-y) * (coordCofres[i+1]-y));
+
+            if (distancia < limite)
+            {
+                limite = distancia;
+            }
+    
+        }
+
+        
+        limite = round(limite);
+
+        if (limite == 0)
+        {// posible cambio 
+            coordCofres.erase(coordCofres.begin() + x);
+            coordCofres.erase(coordCofres.begin() + y);
+            return "Has encontrado un tesoro";
+        }
+        else{
+            if (limite < 10){
+                tab1[x][y] = to_string(limite);
+                return "Has encontrado un tesoro a " + to_string(limite) + "unidades del dispositivo ";
+            }
+            else{
+                tab1[x][y] = "X";
+                return "No has encontrado ningun cofre cerca del sonar";
+            }
+        }
+        return "";
+        
     }
+
+    vector <int> movimientos(){
+        
+        vector <int> movimiento;
+
+        while (true){        
+        
+            cout << "Ingrese una posicion para el eje x: " << endl;
+            cin >> coorX;
+
+            cout << "Ingrese una posicion para el eje y: " << endl;
+            cin >> coorY;
+
+            if(coorX < 0 || coorX > 60){
+                cout << "Ingrese una coordenada valida para el eje x" << endl;
+            }
+            if(coorY < 0 || coorY > 15){
+                cout << "Ingrese una coordenada valida para el eje y" << endl;
+            }
+            else{
+                movimiento.push_back(coorX);
+                movimiento.push_back(coorY);
+                return movimiento;
+            }
+
+
+        }
+    }        
+
+    int jugarOtra(){
+        int op;
+
+        cout << "Desea jugar otra vez? 1. Si / 2. No " << endl;
+        cin >> op;
+
+        return op;
+    }
+
+    
 };
