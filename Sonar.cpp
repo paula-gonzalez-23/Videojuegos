b0a896c8512bae7e2a7859eb02175b71b07d6032
Jesus1102 <jesus60302@gmail.com>
2024-03-27 10:36:26 -0500

    Cree la funcion crear tablero

--- Sonar.cpp
+++ Sonar.cpp
@@ -8,82 +8,70 @@ class Sonar{
 
     public:
     int ANCHO = 60, ALTO = 15, random;
+    string tab1 [60][15] = {};
+    
 
 
     Sonar(){
 
     }
     
-    void crearTablero(int ANCHO, int ALTO){
+    void crearTablero(int ANCHO, int ALTO, string tab1[60][15]){
         
-        string tab1 [ANCHO][ALTO] = {};
+        srand(time(NULL));
+        int random;
 
         for(int i = 0; i < ALTO; i++){
-        for (int j = 0; j < ANCHO; j++)
-        {   
-            random = rand() %2;
-            if(random == 0){
-                tab1[j][i] = "~";
+            for (int j = 0; j < ANCHO; j++)
+            {   
+                random = rand() %2;
+                if(random == 0){
+                    tab1[j][i] = "~";
+                }
+                else{
+                    tab1[j][i] = "^";
+                }
+            
+            }
+        
+        }
+
+    }
+
+
+    void imprimirTablero(string tab1[60][15]){    
+
+        string esp = " ", esp1 = "         ", numbers = "012345678901234567890123456789012345678901234567890123456789";
+        
+        for (int i = 1; i < 6; i++)
+        {
+            esp += (esp1)+ to_string(i);
+        }
+        
+        cout << "   " << esp << endl << "    " << numbers << endl;
+        cout << endl;
+        for(int i = 0; i < 15; i ++){
+            if (i < 10){
+                cout  << " " << i << "  ";
             }
             else{
-                tab1[j][i] = "^";
+                cout  << "" << i << "  ";
             }
-        
-        }
-        
-    }
-
-    }
-    void imprimirtablero(){
-
-    int ANCHO = 60, ALTO = 15, random;
-    srand(time(NULL));
-
-    string tab1 [ANCHO][ALTO] = {}, esp = " ", esp1 = "         ", numbers = "012345678901234567890123456789012345678901234567890123456789";
-    
-    for(int i = 0; i < ALTO; i++){
-        for (int j = 0; j < ANCHO; j++)
-        {   
-            random = rand() %2;
-            if(random == 0){
-                tab1[j][i] = "~";
+            for(int j = 0; j < 60; j ++){
+                cout << tab1[j][i]; 
+            }
+            if (i < 10){
+                cout  << "  " << i << endl;
             }
             else{
-                tab1[j][i] = "^";
+                cout  << " " << i << endl;
             }
-        
+
         }
-    }
-    
-    for (int i = 1; i < 6; i++)
-    {
-        esp += (esp1)+ to_string(i);
-    }
-    
-    cout << "   " << esp << endl << "    " << numbers << endl;
-    cout << endl;
-    for(int i = 0; i < 15; i ++){
-        if (i < 10){
-            cout  << " " << i << "  ";
-        }
-        else{
-            cout  << "" << i << "  ";
-        }
-        for(int j = 0; j < 60; j ++){
-            cout << tab1[j][i]; 
-        }
-        if (i < 10){
-            cout  << "  " << i << endl;
-        }
-        else{
-            cout  << " " << i << endl;
-        }
+        cout << endl;
+        cout << "    " << numbers << endl << "   " << esp << endl ;
 
     }
-    cout << endl;
-    cout << "    " << numbers << endl << "   " << esp << endl ;
-
-}
 
     vector <int> cofres(int numCofres){
         vector <int> coordCofres;
