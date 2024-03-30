bef2da7cbc6c0974868c530a7d9ff73722658cec
Paula Gonzalez <paulav.gonzalez@urosario.edu.co>
2024-03-30 11:33:17 -0500

    Termine el Juego
    Implemente las funciones necesarias para que el ahorcado funcionara de forma correcta, salen las letras incorrectas y el dibujo se va actualizando cada vez, tiene 6 niveles si el usuario adivina cada palabra

--- Ahorcado.cpp
+++ Ahorcado.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <vector>
+#include <string>
 #include <random>
 using namespace std;
 
@@ -6,7 +8,7 @@ class Ahorcado {
 
     private:
 
-    const vector<string> Ahorcado = {
+    const vector<string> AHORCADO = {
         R"(
           +---+
           |   |
@@ -65,18 +67,128 @@ class Ahorcado {
         =========)"
     };
 
-    vector<string> listaNivel1 = {"agua", "cielo", "mar", "tierra", "aire", "fuego", "electricidad"};
-    vector<string> listaNivel2 = {"camiseta", "jean", "zapatos", "medias", "boxers", "esqueleto", "camisa", "gafas", "labial", "manilla"};
-    vector<string> listaNivel3 = {"futbol", "leer", "tejer", "videojuegos", "patinar", "cocinar", "tenis", "gimnasia", "yoga", "pintar"};
-    vector<string> listaNivel4 = {"bosa", "kennedy", "fontibon", "chapinero", "teusaquillo", "engativa", "martires", "suba", "tunjuelito", "usme", "sumapaz"};
-    vector<string> listaNivel5 = {"meximo", "guatemala", "colombia", "brazil", "chile", "ecuador", "argentina", "bolivia", "peru", "honduras", "uruguay", "cuba"};
-    vector<string> listaNivel6 = {"arquitectura", "jurisprudencia", "biologia", "macc", "filosofia", "medicina", "finanzas", "economia", "fisioterapia", "emprendimiento"};
+    vector<vector<string>> palabrasNiveles = {
+      {"agua", "cielo", "mar", "tierra", "aire", "fuego", "electricidad"},
+      {"camiseta", "jean", "zapatos", "medias", "boxers", "esqueleto", "camisa", "gafas", "labial", "manilla"},
+      {"futbol", "leer", "tejer", "videojuegos", "patinar", "cocinar", "tenis", "gimnasia", "yoga", "pintar"},
+      {"bosa", "kennedy", "fontibon", "chapinero", "teusaquillo", "engativa", "martires", "suba", "tunjuelito", "usme", "sumapaz"},
+      {"meximo", "guatemala", "colombia", "brazil", "chile", "ecuador", "argentina", "bolivia", "peru", "honduras", "uruguay", "cuba"},
+      {"arquitectura", "jurisprudencia", "biologia", "macc", "filosofia", "medicina", "finanzas", "economia", "fisioterapia", "emprendimiento"}
+    };
 
-    int nivel = 1;
-    int intentosRestantes = Ahorcado.size() -1;
+    int nivel = 0;
+    int intentosRestantes = AHORCADO.size() -1;
     string palabra;
     string letrasIncorrectas;
     string letrasCorrectas;
 
+  public:
 
+    Ahorcado(){
+
+      srand(time(nullptr));
+    }
+
+    void elegirPalabra(){
+      int indice = rand () % palabrasNiveles[nivel].size();
+      palabra = palabrasNiveles[nivel][indice];
+    }
+
+    void tableroDeJuego(){
+      
+      cout << AHORCADO[AHORCADO.size() - intentosRestantes] << endl;
+      cout << "Nivel: " << nivel + 1 << endl;
+      cout << endl;
+
+      cout << "Las letras incorrectas son: " << endl;
+      for (char letra : letrasIncorrectas){
+        cout << letra << " ";
+      }
+      cout << endl;
+
+      for (char letra : palabra){
+        if (letrasCorrectas.find(letra) != string :: npos){
+          cout << letra << " ";
+        } else {
+          cout << "_ ";
+        }
+      }
+      cout << endl << endl;
+    }
+
+    char elegirLetra(){
+      
+      char letra;
+      cout << "Adivina una letra: " << endl;
+      cin >> letra;
+      return letra;
+    }
+
+    bool inicioJuego(){
+
+      char respuesta;
+      cout << "¿Quieres jugar de nuevo? (s/n): ";
+      cin >> respuesta;
+      return respuesta == 's' || respuesta == 'S';
+    }
+
+    bool letraCorrecta(char letra){
+
+      if(palabra.find(letra) != string :: npos){
+        letrasCorrectas += letra;
+        return true;
+      }else {
+        letrasIncorrectas += letra;
+        return false;
+      }
+    }
+
+    void jugar() {
+
+      bool finJuego = false;
+
+      cout << "*****A H O R C A D O*****" << endl;
+
+      while (!finJuego) {
+        elegirPalabra();
+        intentosRestantes = AHORCADO.size() - 1;
+        letrasCorrectas.clear();
+        letrasIncorrectas.clear();
+
+        while (intentosRestantes > 0){
+          tableroDeJuego();
+
+          char letra = elegirLetra();
+
+          if (palabra.find(letra) != string :: npos){
+            letrasCorrectas += letra;
+          } else {
+            letrasIncorrectas += letra;
+            intentosRestantes --;
+          }
+
+          if (letrasCorrectas.size() == palabra.size()){
+            cout << "¡Haz adivinado la palabra! La palabra era: " << palabra << endl;
+            break;
+          }
+        }
+
+        if (intentosRestantes == 0){
+          cout << "¡Te has quedado sin intentos! La palabra era: " << palabra << endl;
+        }
+
+        if (!inicioJuego()){
+          finJuego = true;
+        }
+      }
+    }
 };
+
+int main () {
+
+  Ahorcado user;
+
+  user.jugar();
+
+  return 0;
+}
