066e16df8497f6f161038c90055333d4db4e3e0e
Paula Gonzalez <147997070+paula-gonzalez-23@users.noreply.github.com>
2024-03-31 19:52:33 -0500

    Create Ejecutable.cpp
    Se pasa el ejecutable de la rama Paula al main

---
+++ Ejecutable.cpp
@@ -0,0 +1,156 @@
+#include <iostream>
+#include "Videojuegos.cpp"
+#include "Ahorcado.cpp"
+using namespace std;
+
+int main (){
+
+    int opcion, opcion1, opcion3, opcion4;
+    string plataforma;
+
+    Videojuegos user;
+    Ahorcado user1;
+
+    do {
+
+        user.menu();
+
+        cout << "Elija la opcion que quiere realizar el dia de hoy" << endl;
+        cin >> opcion;
+
+        switch(opcion){
+            case 1:
+                
+                cout << "Los videojuegos que estan disponibles son: " << endl;
+                cout << "1.Ahorcado" << endl;
+                cout << "2.Sonar" << endl;
+                cout << "Elije tu opcion: " << endl;
+                cin >> opcion1;
+
+                switch(opcion1){
+                    case 1: 
+                        user.verInfo("Ahorcado");
+                        user.iniciarJuego();
+                        user1.jugar();
+                        user.finJuego();
+                        cout << "Tiempo jugado: " << user.gettiempoJugado() << "segundos" << endl;
+                        break;
+                    
+                    case 2:
+                        user.verInfo("Sonar");
+                        user.iniciarJuego();
+                        //llamar la funcion del juego sonar
+                        user.finJuego();
+                        cout << "Tiempo jugado: " << user.gettiempoJugado() << "segundos" << endl;
+                        break;
+                    
+                    default:
+
+                        cout << "Opcion no valida" << endl;
+                        break;
+                }
+                break;
+            
+            case 2:
+
+                user.agregarJuego();
+                break;
+            
+            case 3:
+
+                user.agregarCategoria();
+                break;
+            
+            case 4:
+
+                user.agregarListaFavoritos();
+                break;
+            
+            case 5:
+
+                user.mostrarListaFavoritos();
+                break;
+            
+            case 6:
+
+                cout << "Elija porque quiere consultar y mostrar la lista de juegos: " << endl;
+                cout << "1.Ordenados por titulo" << endl;
+                cout << "2.Ordenados por anio de Lanzamiento" << endl;
+                cout << "3.Filtrados por una plataforma espeifica" << endl;
+                cout << "Elije tu opcion" << endl;
+                cin >> opcion3;
+
+                switch(opcion3){
+                    case 1:
+
+                        cout << "Lista de videojuegos ordenados por titulo" << endl;
+                        user.ordenadosPorTitulo(multilista);
+                        break;
+                    
+                    case 2:
+                    
+                        cout << "Lista de videojuegos ordenados por anio de lanzamiento" << endl;
+                        user.ordenadosPorAnioLanzamiento(multilista);
+                        break;
+                    
+                    case 3:
+
+                        cout << "Ingrese la plataforma para filtrar los videojuegos: " << endl;
+                        cin >> plataforma;
+
+                        cout << "Lista de videjuegos para la plataforma " << plataforma << ":" << endl;
+                        user.mostrarJuegosPorPlataforma(multilista,plataforma);
+                        break;
+                    
+                    default:
+
+                        cout << "Opcion no valida" << endl;
+                        break;
+                
+                break;
+            
+            case 7:
+
+                cout << "Ingrese la opcion por la quiere ordenar los video juegos: " << endl;
+                cout << "1.Por jugador" << endl;
+                cout << "2.Por tiempo jugado" << endl;
+                cout << "3.Por juego mas jugado" << endl;
+                cout << "Elije tu opcion: " << endl;
+                cin >> opcion4;
+
+                switch(opcion4){
+                    case 1:
+
+                        user.ordenarPorJugador();
+                        break;
+
+                    case 2:
+
+                        user.ordenarPorTiempoJugado();
+                        break;
+
+                    case 3:
+
+                        user.ordenarPorJuegoMasJugado();
+                        break;
+                    
+                    default:
+
+                        cout << "Opcion no valida" << endl;
+                        break;
+                }
+
+                break;
+            
+            default:
+
+                cout << "Opcion no valida" << endl;
+                break;
+
+        }
+
+    } while (opcion != 8);
+
+
+    return 0;
+}
